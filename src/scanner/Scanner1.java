5d81045ca139196bcf8bf7962faa9beb09d3e21d
sbag0793 <sbag0793@gmail.com>
2024-07-22 21:06:50 +0900

    ADD: 1 files

---
+++ src/scanner/Scanner1.java
@@ -0,0 +1,11 @@
+package scanner;
+
+import java.util.Scanner;
+
+public class Scanner1 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("문자열을 입력하세요.:");
+    }
+}
